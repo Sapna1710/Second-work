728f4f800afda9c70ff94f919617e6d2056de033
nepali <nepali@DESKTOP-373AQL1>
2017-02-08 14:19:23 +0530

    start of file

---
+++ third/Sum.java
@@ -0,0 +1,12 @@
+class Sum
+{
+	int a,b;
+	int sum=0;
+	public static void main(string args[])
+	{
+		sum = (a+b);
+		system.out.println("sum is ", + sum);
+		system.out.println("made some changes");
+	}
+	
+}
